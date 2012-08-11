514eaab1bb93900fbba32d836c534393b9e2f886
Curtis Jones <curtis.jones@gmail.com>
2012-08-11 15:13:17 -0400

    IP address region allocations.

---
+++ other/regions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# APNIC - Asia Pacific
+wget ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-latest
+
+# AfriNIC - Africa
+wget ftp://ftp.apnic.net/pub/stats/afrinic/delegated-afrinic-latest
+
+# RIPE NCC - Europe, Middle East, Central Asia
+wget ftp://ftp.ripe.net/ripe/stats/delegated-ripencc-latest
+
+# LACNIC - Latin America
+wget ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest
+
+# ARIN - North America
+wget ftp://ftp.arin.net/pub/stats/arin/delegated-arin-latest
